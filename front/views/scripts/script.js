6950210941cacf4da311ece49e4bcf3584be7a91
brwnitz <brunotrs16@gmail.com>
2022-11-22 10:44:42 -0300

    Modal Cadastro de perdido

--- front/views/scripts/script.js
+++ front/views/scripts/script.js
@@ -1,11 +1,1 @@
-var modalCadStart =  document.getElementById("modalCadStart");
-var btnOpenCadStart = document.getElementById("btnOpenCadStart");
-var closeCadStart = document.getElementById("closeCadStart");
 
-btnOpenCadStart.onclick = function(){
-    modalCadStart.style.display = "block";
-}
-
-closeCadStart.onclick = function(){
-    modalCadStart.style.display = "none";
-}
